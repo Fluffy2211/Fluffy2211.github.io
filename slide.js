1a58839677fa5a9629bfbb83e52ec3ee744f03c2
Fluffy221 <58748353+Fluffy221@users.noreply.github.com>
2020-04-14 14:49:19 -0500

    Add files via upload

--- slide.js
+++ slide.js
@@ -1,5 +1,7 @@
 let slideIndex = 1;
 showDivs(slideIndex);
+showDivs1(slideIndex);
+showDivs2(slideIndex);
 
 function plusDivs(n) {
     showDivs(slideIndex += n);
@@ -24,6 +26,22 @@ function plusDivs1(n) {
 function showDivs1(n) {
     let i;
     let x = document.getElementsByClassName("mySlides1");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+//slide 3
+function plusDivs2(n) {
+    showDivs2(slideIndex += n);
+}
+
+function showDivs2(n) {
+    let i;
+    let x = document.getElementsByClassName("mySlides2");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
